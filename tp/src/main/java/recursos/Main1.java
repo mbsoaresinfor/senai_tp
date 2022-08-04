e98861ecfc18a433a550fb7fe87d81fee0c549ac
Faculdade SENAI <fatec.senai@acad692.com.br>
2022-08-03 22:09:12 -0300

    aula 2

--- tp/src/main/java/recursos/Main1.java
+++ tp/src/main/java/recursos/Main1.java
@@ -8,10 +8,10 @@ package recursos;
 public class Main1 {
 
 	public static void main(String[] args) {
-		CalculadoraPag c = new CalculadoraPag("maria",
-                        30, "26/06/1990", 500);
-                float ret = c.retornaCalculoFuncionario();
-                System.out.println("salario: " + ret);
+//		CalculadoraPag c = new CalculadoraPag("maria",
+//                        30, "26/06/1990", 500);
+//                float ret = c.retornaCalculoFuncionario();
+//                System.out.println("salario: " + ret);
 	}
 
 }
